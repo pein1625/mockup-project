fca5baf93604deba39fc8b9bf8f23e94cbf85b08
ha <pein1625@gmail.com>
2018-01-24 12:53:44 +0700

    create-common-footer

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,3 @@
 //=require jquery/dist/jquery.min.js
-//=require popper.js/dist/umd/popper.js
-//=require bootstrap/dist/js/bootstrap.js
 //=require owl.carousel/dist/owl.carousel.js
 //=require app.js
